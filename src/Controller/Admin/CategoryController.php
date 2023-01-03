8ff8e81e054064e28d976c05a95d3cff5c7bea9c
AlexPovlov <sasha.povlov47@gmail.com>
2023-01-03 13:21:49 +0300

    category

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/ehe', name: 'app_main')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MainController.php',
+        ]);
+    }
+}
